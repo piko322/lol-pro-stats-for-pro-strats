ff39548bd4886a2db87ddaccde770fe473428d98
piko322 <dhilansunandabong@gmail.com>
2023-09-06 12:33:27 +0700

    bugfixed get_top method

--- RiotAnalyzer.py
+++ RiotAnalyzer.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import time
 
 class RiotAnalyzer:
     """A class to get data from the Riot API
@@ -30,8 +31,8 @@ class RiotAnalyzer:
         }
         self.url_template = "https://{region_code}.api.riotgames.com/lol/{endpoint}?{query}"
     
-    def get_leaderboard_raw(self, queue, rank:str, region=None):
-        """Get the leaderboard for a specific queue, tier, and division.
+    def get_leaderboard_raw(self, queue, rank:str, region=None, page=1):
+        """Gets a certain page of the leaderboard for a specific queue, tier, and division. NOTE: It is not sorted.
         Returns the JSON response from the Riot API.
 
         Args:
@@ -66,33 +67,89 @@ class RiotAnalyzer:
                 
         endpoint = f"league/v4/entries/{queue}/{tier}/{division}"
         
-        url = self.url_template.format(region_code=self.region_code, endpoint=endpoint, query="page=1")
+        url = self.url_template.format(region_code=self.region_code, endpoint=endpoint, query=f"page={page}")
         response = requests.get(url, headers=self.header)
         
-        if response.status_code != 200:
+        if response.status_code == 429:
+            # If the request is rate limited, wait for the specified time and try again
+            # Check if response.headers["Retry-After"] exists, if not, wait for 10 seconds
+            if "Retry-After" not in response.headers:
+                print("Rate limited, retrying in 30 seconds")
+                time.sleep(30)
+                return self.get_leaderboard_raw(queue, rank, region, page)
+            else:
+                retry_after = response.headers["Retry-After"]
+                print(f"Rate limited, retrying in {retry_after} seconds")
+                time.sleep(retry_after)
+                return self.get_leaderboard_raw(queue, rank, region, page)
+            
+        elif response.status_code == 503:
+            # If the server is unavailable, wait for 30 seconds and try again
+            print("Server unavailable, retrying in 30 seconds")
+            time.sleep(30)
+            return self.get_leaderboard_raw(queue, rank, region, page)
+        
+        elif response.status_code != 200:
             raise Exception(
                 f"Error {response.status_code} when querying leaderboard")
         data = response.json()
 
         return data
 
-    def get_top_20(self, data=None, queue=None, rank=None, region=None):
+    def get_top(self, queue=None, rank=None, region="NA1", keep_top:int=20, start_page:int=1, page_limit:int=99999):
         """Takes in the JSON data from get_leaderboard_raw and converts it to a Pandas DataFrame containing the following columns in order:
         tier, division, rank, summonerId, summonerName, leaguePoints, wins, losses, veteran, inactive, freshBlood, queueType
 
         Args:
             data (json bytearray): The JSON data from get_leaderboard_raw
         """
-        # If data is not provided, get it from the Riot API
-        if data is None:
-            data = self.get_leaderboard_raw(queue, rank, region)
+        # Use a while loop to get all the pages of the leaderboard
+        # For each page, take only the top 20 highest "leaguePoint" and discard the rest
+        # On each subsequent iteration, also take the top 20, append that to the DataFrame, and again keep only the top 20
+        # So each iteration will add 20 rows to the DataFrame, but then discard all but the top 20 rows among the 40 rows, by leaguePoints
+        # This is done because the Riot API only returns 200 entries per page, and there are more than 200 entries in the leaderboard
+        # So we have to get all the pages and then sort them ourselves
+        if queue is None:
+            raise Exception("Queue must be specified")
+        if rank is None:
+            raise Exception("Rank must be specified")
         
-        df = pd.DataFrame(data)
-        df = df[["tier", "rank", "summonerName", "leaguePoints", "wins", "losses", "veteran", "inactive", "freshBlood", "hotStreak",'queueType', "summonerId"]]
+        page = start_page
+        total_entries = 0
+        result_df = pd.DataFrame()
+        print("Queried pages: ", end="")
+        while True and (page <= page_limit + start_page - 1):
+            print(page, end=", ")
+            # Add line break every 10 pages
+            if page % 10 == 0:
+                print()
+            data = self.get_leaderboard_raw(queue, rank, region, page)
+            # Exit the loop if there is no more data
+            if data == []:
+                break
+            df = pd.DataFrame(data)
+            
+            # Add a new "Winrate" column to the DataFrame
+            df["winrate"] = df["wins"] / (df["wins"] + df["losses"])
+           
+            # Add the total number of entries in the leaderboard to the total_entries variable
+            total_entries += len(df)
+            
+            # Concatenate the new DataFrame to the result DataFrame
+            result_df = pd.concat([result_df, df])
+            
+            # Sort the result DataFrame by leaguePoints, then winrate
+            result_df = result_df.sort_values(by=["leaguePoints", "winrate"], ascending=False)
+           
+            # Keep only the top x amount rows (default 20)
+            result_df = result_df.head(keep_top)
+            page += 1
         
-        # Add a "Leaderboard Rank" column to the DataFrame and sort by it
-        df["Leaderboard Rank"] = df["leaguePoints"].rank(method='dense', ascending=False)
-        df = df.set_index("Leaderboard Rank")
-        df = df.sort_index()
-
-        return df.head(20)
+        # Reset the index of the result DataFrame, drop the old index,
+        # and rename the new index to "Rank"
+        result_df = result_df.reset_index(drop=True)
+        result_df.index.name = "Rank"
+        result_df.index += 1
+        print("Total entries:", total_entries)
+    
+        return result_df
